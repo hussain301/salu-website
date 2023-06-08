726c22ceab1b651c16db775a82026ce3269be7c0
hussain <hussainnariwal@gmail.com>
2023-06-08 20:20:23 +0500

    cdn to false

--- sanity.js
+++ sanity.js
@@ -1,12 +1,14 @@
-import { createClient } from "next-sanity";
-import createImageUrlBuilder from "@sanity/image-url";
+import { createClient } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
-  apiVersion: "2021-03-25",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: '2021-03-25',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: false,
+  useCdn:false,
   
 }
 
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
